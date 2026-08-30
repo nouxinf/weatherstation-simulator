161bcee70c99f8cd0951539f5e748d8c6172e289
nouxinf <180769756+nouxinf@users.noreply.github.com>
2026-08-30 17:01:40 +0100

    potentially fix server??

--- start_server.py
+++ start_server.py
@@ -3,34 +3,54 @@
 from http.server import ThreadingHTTPServer, SimpleHTTPRequestHandler, test
 import os
 
+
 class CORSRequestHandler(SimpleHTTPRequestHandler):
     def end_headers(self):
-        self.send_header('Access-Control-Allow-Origin', '*')
-        self.send_header('Cross-Origin-Opener-Policy', 'same-origin')
-        self.send_header('Cross-Origin-Embedder-Policy', 'require-corp')
+        self.send_header("Access-Control-Allow-Origin", "*")
+        self.send_header("Cross-Origin-Opener-Policy", "same-origin")
+        self.send_header("Cross-Origin-Embedder-Policy", "require-corp")
         SimpleHTTPRequestHandler.end_headers(self)
 
+    def guess_type(self, path):
+        if path.endswith(".js"):
+            return "text/javascript"
+        return super().guess_type(path)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     import argparse
     import contextlib
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--cross', action='store_true',
-                        help='run as secure cross origin server')
-    parser.add_argument('-b', '--bind', metavar='ADDRESS',
-                        help='bind to this address '
-                             '(default: all interfaces)')
-    parser.add_argument('-d', '--directory', default=os.getcwd(),
-                        help='serve this directory '
-                             '(default: current directory)')
-    parser.add_argument('-p', '--protocol', metavar='VERSION',
-                        default='HTTP/1.0',
-                        help='conform to this HTTP version '
-                             '(default: %(default)s)')
-    parser.add_argument('port', default=8000, type=int, nargs='?',
-                        help='bind to this port '
-                             '(default: %(default)s)')
+    parser.add_argument(
+        "--cross", action="store_true", help="run as secure cross origin server"
+    )
+    parser.add_argument(
+        "-b",
+        "--bind",
+        metavar="ADDRESS",
+        help="bind to this address " "(default: all interfaces)",
+    )
+    parser.add_argument(
+        "-d",
+        "--directory",
+        default=os.getcwd(),
+        help="serve this directory " "(default: current directory)",
+    )
+    parser.add_argument(
+        "-p",
+        "--protocol",
+        metavar="VERSION",
+        default="HTTP/1.0",
+        help="conform to this HTTP version " "(default: %(default)s)",
+    )
+    parser.add_argument(
+        "port",
+        default=8000,
+        type=int,
+        nargs="?",
+        help="bind to this port " "(default: %(default)s)",
+    )
     args = parser.parse_args()
     if args.cross:
         handler_class = CORSRequestHandler
@@ -43,13 +63,13 @@ if __name__ == '__main__':
         def server_bind(self):
             # suppress exception when protocol is IPv4
             with contextlib.suppress(Exception):
-                self.socket.setsockopt(
-                    socket.IPPROTO_IPV6, socket.IPV6_V6ONLY, 0)
+                self.socket.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_V6ONLY, 0)
             return super().server_bind()
 
         def finish_request(self, request, client_address):
-            self.RequestHandlerClass(request, client_address, self,
-                                     directory=args.directory)
+            self.RequestHandlerClass(
+                request, client_address, self, directory=args.directory
+            )
 
     test(
         HandlerClass=handler_class,
